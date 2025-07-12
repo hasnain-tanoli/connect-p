9755170697bfc7e3379f4199176b91a7dbddb729
Hasnain Tanoli <hasnaintanoli.official@gmail.com>
2025-07-12 12:24:03 +0500

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
-console.log("API Base URL:", BASE_URL);
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
